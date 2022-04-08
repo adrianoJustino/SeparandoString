1e7dfd64468b1d754ad417f66c6f074d9bae793b
adria <adria@DOOMGUY-PC>
2022-04-08 02:27:11 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "abacaxi";
+            int vogal = 0;
+            int consoante = 0;
+            foreach (char item in a)
+            {
+                
+                Console.WriteLine(item);
+                if (item == 'a' || item == 'i')
+                    vogal += 1;
+                else consoante += 1;
+                Console.WriteLine("qtd vogais: "+vogal);
+                Console.WriteLine("qtd consoantes: "+consoante);
+            }
+        }
+    }
+}
